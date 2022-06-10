b1bd60700a8f9071d990d1bdd2fb55d870d9b760
Yury Klimchuk <hydro.yura@gmail.com>
2022-06-11 01:23:23 +0300

    task solved

--- movies_app/js/script.js
+++ movies_app/js/script.js
@@ -1,1 +1,24 @@
 alert("Hellow world!");
+
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посомтрели?", "");
+
+const personalMovieDB = {
+    
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("Ваша оценка данного фильма?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("Ваша оценка данного фильма?", "");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
